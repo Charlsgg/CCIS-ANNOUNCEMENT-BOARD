f71cd1ee7f5edcb08905ee27d01d503840b569c1
Charlsgg <mcklienxx@gmail.com>
2026-03-18 11:27:21 +0800

    dsads

--- CCIS-MIRROR/vite.config.ts
+++ CCIS-MIRROR/vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
     host: '0.0.0.0', 
     port: 5173,
     hmr: {
-      // Update this to match your current network IP
+      // Update this to match your current network IPasdasda
       host: '192.168.200.112', 
       port: 5173,
     },
