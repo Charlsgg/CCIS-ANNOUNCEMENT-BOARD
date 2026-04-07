9bcb636df9fb48289393c817455c434095cf8e27
Charlsgg <mcklienxx@gmail.com>
2026-04-07 14:05:59 +0800

    Configure Axios to send secure cookies and set base URL for Vercel domain

--- CCIS-MIRROR/resources/js/bootstrap.js
+++ CCIS-MIRROR/resources/js/bootstrap.js
@@ -2,3 +2,9 @@ import axios from 'axios';
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+// 1. CRITICAL: Tell Axios to send secure cookies with every request
+window.axios.defaults.withCredentials = true;
+
+// 2. OPTIONAL BUT RECOMMENDED: Tell Axios to use your current Vercel domain automatically
+window.axios.defaults.baseURL = '/';
